a552ef7fd2f1174542c8444e4b58175b8dd68523
Michaaaal <134453178+Michaaaal@users.noreply.github.com>
2023-05-31 19:28:49 +0200

    Update Monster.java

--- src/main/java/efs.task.oop/Monster.java
+++ src/main/java/efs.task.oop/Monster.java
@@ -1,49 +1,27 @@
 package efs.task.oop;
 
-public abstract class Monster implements Fighter{
-
-    protected int health;
-    protected int damage;
-    protected boolean isAlive;
+public abstract class Monster implements Fighter {
+    private int health;
+    private final int damage;
 
     public Monster(int health, int damage) {
         this.health = health;
         this.damage = damage;
-        this.isAlive = true;
     }
+
+    public void attack(Fighter victim) {
+        victim.takeHit(damage);
+    }
+    public void takeHit(int damage) {
+        if (health > damage) {
+            health -= damage;
+        }
+        else {
+            health = 0;
+        }
+    }
+
     public int getHealth() {
         return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-
-    public boolean isAlive() {
-        if(this.health>0){
-            isAlive=true;
-        }else{
-            isAlive=false;
-        }
-        return isAlive;
-    }
-
-    @Override
-    public void attack(Fighter victim) {
-        victim.takeHit(this.damage);
-    }
-
-    @Override
-    public void takeHit(int damage) {
-        health-=damage;
     }
 }
